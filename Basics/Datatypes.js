faabd36d7c16f468c1ff0da8131d3419ed0391da
Taniya Taragi <taniyataragi10@gmail.com>
2025-03-11 09:33:41 +0000

    Variables and Datatypes completed

---
+++ Basics/Datatypes.js
@@ -0,0 +1,17 @@
+"user strict";//consider this  js code as newer version .It is not neccessary to mention this.
+//alert("hello") error bcz we are using node js not browser
+//dc39  and mdn documentation
+   //primitve data types
+let name="taniya" //string
+let age=18 //number
+let islogged=false //boolean
+//bigint
+let value=null //null=>stand alone value
+let state; //undefined
+//symbol=>unique
+
+//object
+
+console.log(typeof(islogged))//boolean
+console.log(typeof(undefined))//undefined
+console.log(typeof(value));//object
---
+++ Basics/Variables.js
@@ -0,0 +1,14 @@
+const studentId=230421217 //you can either use semicolon at the end or not
+var name="TaniyaTaragi"
+let gender="Female"
+hobby="Cricket"
+let age;//undefined
+//studentId=24//error
+console.log(studentId);
+console.log(name);
+console.table([name,gender,hobby,age])
+
+/* var- avoid using it as it has issues in block and functional scope */
+
+
+
